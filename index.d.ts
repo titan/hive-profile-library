88a7a7e3d18e63c03885a27ef4f95bfabe5dcb70
Titan <howay.tan@gmail.com>
2017-04-20 10:14:16 +0800

    Initial import

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+export interface User {
+    id: string;
+    openid: string;
+    passsword: string;
+    name: string;
+    gender: string;
+    identity_no: string;
+    phone: string;
+    nickname: string;
+    portrait: string;
+    pnrid: string;
+    ticket: string;
+    tender_opened: boolean;
+    insured: string;
+    inviter: string;
+}
